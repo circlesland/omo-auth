1ab897037476362df89d8ff758ee207bf0adbf0e
jaensen <no@email.comm>
2020-09-11 14:04:56 +0200

    moved all the docker stuff to the "fuckup" dir.

--- auth/data/src/prisma.ts
+++ auth/data/src/prisma.ts
@@ -3,7 +3,7 @@ import {PrismaClient} from "@prisma/client";
 export const prisma: PrismaClient = new PrismaClient({
     datasources: {
         db: {
-            url: process.env.POSTGRES_CONNECTION_URL
+            url: "postgresql://" + process.env.POSTGRES_USER + ":" + process.env.POSTGRES_PASSWORD + "@" + process.env.POSTGRES_HOST + ":5432/" + process.env.POSTGRES_DB + "?schema=public"
         }
     }
 });
