4e1fca25819432cd6233f974a14a501d77122502
jaensen <no@email.comm>
2020-09-11 16:13:57 +0200

    * Added a missing ENV var * Added a simple static page to submit the email address to the auth-service

--- auth/mailer/src/templates/login.ts
+++ auth/mailer/src/templates/login.ts
@@ -3,7 +3,7 @@ import {Template} from "../template";
 export const login:Template = {
     subject: "Your OMO.earth login magic-link",
     bodyPlain: `Please click the link below to sign-in:
-https://omo.earth/login?code={{challenge}}`,
+https://auth.omo.earth/static/login?code={{challenge}}`,
     bodyHtml: `Please click the link below to sign-in:<br/>
-<a href="https://omo.earth/login?code={{challenge}}">Sign-in</a>`
+<a href="https://auth.omo.earth/static/login?code={{challenge}}">Sign-in</a>`
 }
